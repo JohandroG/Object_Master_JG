9d1ed8b3b51f4e87b291fc129fc7d32b6c1da240
Johandro Gonzalez <88999789+JohandroG@users.noreply.github.com>
2021-10-20 12:19:56 -0600

    New changes

--- src/com/objectmasterTest/HumanTest.java
+++ src/com/objectmasterTest/HumanTest.java
@@ -1,5 +1,14 @@
 package com.objectmasterTest;
+import com.objectmasterclasses.*;
 
 public class HumanTest {
 
+	public static void main(String[] args) {
+	Human juanito =  new Human();
+	Human pedro =  new Human();
+	
+	pedro.attack(juanito);
+	juanito.printStadistics();
+	
+	}
 }
--- src/com/objectmasterclasses/Human.java
+++ src/com/objectmasterclasses/Human.java
@@ -1,5 +1,26 @@
 package com.objectmasterclasses;
 
 public class Human {
-
+	protected int strength = 3;
+	protected int stealth = 3;
+	protected int intelligence = 3;
+	protected int health = 100;
+	
+	
+	public void attack(Human objetive) {
+		objetive.health -= this.strength;
+	}
+	
+	public void printStadistics() {
+		System.out.println("Strength: " + this.strength + " Stealth: " + this.stealth + " Intelligence: " + this.intelligence + " Health: " + this.health );
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
 }
